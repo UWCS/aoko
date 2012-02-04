ac5ba0bdfc078698dfb9ad50090ee8d1352f4e7e
mrwilson <csuiav@warwick.ac.uk>
2012-02-04 13:00:10 +0000

    Less shunting with @PostConstruct

--- src/main/java/uk/co/probablyfine/aoko/player/MplayerPlayer.java
+++ src/main/java/uk/co/probablyfine/aoko/player/MplayerPlayer.java
@@ -16,12 +16,26 @@ public class MplayerPlayer {
 	@PostConstruct
 	public void playTracks() throws InterruptedException {
 		
-		while (true) {
-			Thread.sleep(2000);
+		new Thread(new Runnable() {
 			
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					System.out.println("Here!");
 
-			
-		}
+					
+				}
+				
+			}
+		}).start();
+		
+		
 		
 		
 	}

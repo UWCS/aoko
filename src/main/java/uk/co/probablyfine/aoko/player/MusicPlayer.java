20bbbeea2492288b5ac07a6cf5103f344e9c9829
Alex Wilson <csuiav@warwick.ac.uk>
2012-06-22 13:47:44 +0100

    More templating.

--- src/main/java/uk/co/probablyfine/aoko/player/MusicPlayer.java
+++ src/main/java/uk/co/probablyfine/aoko/player/MusicPlayer.java
@@ -1,13 +1,11 @@
 package uk.co.probablyfine.aoko.player;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
--- src/main/java/uk/co/probablyfine/aoko/plugins/ScrobblerPlugin.java
+++ src/main/java/uk/co/probablyfine/aoko/plugins/ScrobblerPlugin.java
@@ -4,21 +4,34 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Value;
 
 import uk.co.probablyfine.aoko.domain.QueueItem;
 
 @Aspect
 public class ScrobblerPlugin {
 
+	@Value("${scrobble.name}")
+	String username;
+	
+	@Value("${scrobble.secret}")
+	String secret;
+	
+	@Value("${scrobble.password}")
+	String password;
+	
+	@Value("${scrobble.apikey}")
+	String apiKey;
+	
 	@Pointcut("execution(* uk.co.probablyfine.aoko.dao.QueueItemDao.finishedPlaying(..))")
 	public void trackFinishedPlaying() {}
 	
 	@Around("trackFinishedPlaying()")
 	public Object logDao(ProceedingJoinPoint pjp) throws Throwable {
 		
-		QueueItem qi = (QueueItem) pjp.getArgs()[0];
+		final QueueItem qi = (QueueItem) pjp.getArgs()[0];
 		
-		System.out.println("Oh look, I'm scrobbling "+qi.getFile().getMetaData().get("originalname"));
+		System.out.println("Scrobbling "+qi.toString()+" to "+username);
 		
 		return pjp.proceed();
 	}
